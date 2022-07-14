f39b4a95b156d9f72f0aeaa4060339f2f0332b87
Nahuel <nahuelmatiasbenitez@gmail.com>
2022-07-14 11:05:38 -0300

    modify footer

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -35,10 +35,10 @@ const Footer = () => {
           <p>085-132567</p>
           <p>info@payme.net</p>
         </div>
-        <div className='gpt3__footer-copyright'>
+      </div>
+      <div className='gpt3__footer-copyright'>
           <p>© 2021 GPT-3. All rights reserved.</p>
         </div>
-      </div>
     </div>
   );
 }
